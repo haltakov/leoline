3bd4cb255b22c0065ec7973ff016e12bcc265b36
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2024-06-24 21:54:00 +0200

    feat: base app setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-blue-200">
+      <div className="flex justify-center">
+        <div className="max-w-[1000px] bg-slate-100 border border-blue-300 p-4 m-8 rounded shadow-lg">
+          <div>Hey, I&apos;m Leoline. What is your name?</div>
+        </div>
+      </div>
+    </main>
+  );
+}
