5010c5c88c88c957ecb1be21cae096a40cf1e217
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2024-09-14 23:19:19 +0200

    feat: implement auth with magic link

--- src/frontend/account/components/Login.tsx
+++ src/frontend/account/components/Login.tsx
@@ -7,14 +7,26 @@ const Login = () => {
     <div className="space-y-20">
       <Heading>Create an account or sign in</Heading>
 
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center space-y-16 px-16">
         <form
+          className="w-full flex flex-col"
           action={async () => {
             "use server";
             await signIn("google");
           }}
         >
           <Button>Sign in with Google</Button>
+        </form>
+
+        <form
+          className="flex flex-col space-y-2 w-full"
+          action={async (formData) => {
+            "use server";
+            await signIn("resend", formData);
+          }}
+        >
+          <input className="border-2 px-4 py-2 rounded-md" type="email" name="email" placeholder="Email" />
+          <Button>Sign in with email</Button>
         </form>
       </div>
     </div>
--- src/frontend/auth/index.ts
+++ src/frontend/auth/index.ts
@@ -2,8 +2,9 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import prisma from "@/backend/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import Resend from "next-auth/providers/resend";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [Google, Resend],
 });
